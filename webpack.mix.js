cca13b57c67ddd23451c42e4cb9e632e598a974a
Matt Stauffer <matt@tighten.co>
2018-05-01 11:38:36 -0700

    Add Tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
+let tailwindcss = require("tailwindcss");
+require("laravel-mix-purgecss");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +13,28 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+  .js("resources/assets/js/app.js", "public/js")
+  .postCss("resources/assets/css/main.css", "public/css", [
+    tailwindcss("./tailwind.js")
+  ])
+  .purgeCss();
+
+if (mix.inProduction()) {
+  mix.version();
+}
+
+// If you want to use LESS for your preprocessing
+// mix.less('resources/assets/less/main.less', 'public/css')
+//   .options({
+//     postCss: [
+//       tailwindcss('./tailwind.js'),
+//     ]
+//   })
+
+// If you want to use SASS for preprocessing
+// mix.sass('resources/assets/sass/app.scss', 'public/css')
+//    .options({
+//       processCssUrls: false,
+//       postCss: [ tailwindcss('tailwind.js') ],
+//    });
