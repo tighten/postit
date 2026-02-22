c6f9aab046ddc0b1ea1edff10eb9e8dad486230b
Matt Stauffer <matt@tighten.co>
2026-02-22 16:45:00 -0500

    Fix frontend build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue2';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js',
+        },
+    },
     plugins: [
         laravel({
             input: [
