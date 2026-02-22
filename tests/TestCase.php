c8a9b0cc158ba00cce9bfabcba3c1f658eb9a277
Shift <shift@laravelshift.com>
2026-02-22 19:33:24 +0000

    Remove `CreatesApplication` testing trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,5 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, \Illuminate\Foundation\Testing\DatabaseMigrations;
+    use \Illuminate\Foundation\Testing\DatabaseMigrations;
 }
