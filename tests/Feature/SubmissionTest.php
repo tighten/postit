281fbc6bd6b4abdc4c7e5cf2d12ca3ad4a477d98
Shift <shift@laravelshift.com>
2026-03-01 18:23:01 +0000

    Convert test cases

--- tests/Feature/SubmissionTest.php
+++ tests/Feature/SubmissionTest.php
@@ -1,49 +1,41 @@
 <?php
 
-namespace Tests\Feature;
-
 use App\Post;
 use App\Submission;
 use App\Target;
-use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-class SubmissionTest extends TestCase
-{
-    #[Test]
-    public function it_can_create_a_submission(): void
-    {
-        $response = $this->post(route('api.submissions.store'), [
-            'target_id' => $targetId = Target::factory()->create(['id' => 5])->id,
-            'post_id' => $sourceId = Post::factory()->create(['id' => 12])->id,
-        ]);
+uses(Tests\TestCase::class);
 
-        $response->assertStatus(200);
+it('can create a submission', function () {
+    $response = $this->post(route('api.submissions.store'), [
+        'target_id' => $targetId = Target::factory()->create(['id' => 5])->id,
+        'post_id' => $sourceId = Post::factory()->create(['id' => 12])->id,
+    ]);
 
-        $this->assertCount(1, Submission::get());
-        $submission = Submission::first();
+    $response->assertStatus(200);
 
-        $this->assertEquals($targetId, $submission->target_id);
-        $this->assertEquals($sourceId, $submission->post_id);
-    }
+    $this->assertCount(1, Submission::get());
+    $submission = Submission::first();
 
-    #[Test]
-    public function it_can_delete_a_submission(): void
-    {
-        $submission = Submission::factory()->create();
-        $this->assertCount(1, Submission::get());
-        $this->assertCount(1, Post::get());
-        $this->assertCount(1, Target::get());
+    $this->assertEquals($targetId, $submission->target_id);
+    $this->assertEquals($sourceId, $submission->post_id);
+});
 
-        $response = $this->delete(route('api.submissions.destroy', [
-            'target_id' => $submission->target_id,
-            'post_id' => $submission->post_id,
-        ]));
+it('can delete a submission', function () {
+    $submission = Submission::factory()->create();
+    $this->assertCount(1, Submission::get());
+    $this->assertCount(1, Post::get());
+    $this->assertCount(1, Target::get());
 
-        $response->assertOk();
+    $response = $this->delete(route('api.submissions.destroy', [
+        'target_id' => $submission->target_id,
+        'post_id' => $submission->post_id,
+    ]));
 
-        $this->assertCount(0, Submission::get());
-        $this->assertCount(1, Post::get());
-        $this->assertCount(1, Target::get());
-    }
-}
+    $response->assertOk();
+
+    $this->assertCount(0, Submission::get());
+    $this->assertCount(1, Post::get());
+    $this->assertCount(1, Target::get());
+});
